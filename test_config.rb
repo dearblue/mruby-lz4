217a0ee190e0087116a8956de05a414b9770b33e
dearblue <dearblue@users.osdn.me>
2022-08-20 17:23:48 +0900

    テスト構成の更新

--- test_config.rb
+++ test_config.rb
@@ -1,10 +1,28 @@
 MRuby::Lockfile.disable rescue nil
 
+unless MRuby::Build.method_defined? :defines
+  module MRuby
+    class Build
+      def defines
+        conf = self
+        proxy = Object.new
+        proxy.define_singleton_method(:<<, &->(*defs) {
+          conf.compilers.each { |cc| cc.defines << defs }
+          proxy
+        })
+        proxy
+      end
+    end
+  end
+end
+
 MRuby::Build.new("host", "build") do |conf|
   toolchain :clang
 
   enable_debug
   enable_test
+
+  defines << %w(MRB_STR_LENGTH_MAX='(16UL<<20)')
 
   gem core: "mruby-print"
   gem core: "mruby-bin-mirb"
@@ -18,6 +36,8 @@ MRuby::Build.new("host++", "build") do |conf|
   enable_debug
   enable_test
   enable_cxx_abi
+
+  defines << %w(MRB_STR_LENGTH_MAX='(16UL<<20)')
 
   cc.flags << "-std=c++11"
   cxx.flags << "-std=c++11"
